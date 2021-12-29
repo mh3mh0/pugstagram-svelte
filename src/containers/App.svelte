8dbdd5f438cb6c060a1794914bb89887bf1a7b5e
JOHN RUIZ <jonxca@icloud.com>
2021-12-29 10:04:41 +0100

    first commit ever

---
+++ src/containers/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+
+  let data = {};
+
+  const API =
+    "https://api-pugstagram.herokuapp.com/?nickname=MiguelAngelMP&name=MiguelAngel";
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
+<Header />
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,300&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
